47d4d356f56bad8508fac8cda48a3c839ceacdcd
Chris Yates <chris@clyates.plus.com>
2024-01-07 17:01:41 +0000

    Update tests

--- tests/Unit/ClassDiagramTest.php
+++ tests/Unit/ClassDiagramTest.php
@@ -117,10 +117,16 @@ test('classDiagram with relationship', function (RelationshipType $relationship)
 ;
 
 test('classDiagram with everything', function () {
-    $class1 = new Classs(name: CLASS_NAME . '1', namespace: CLASS_NAMESPACE . '1');
-    $class2 = new Classs(name: CLASS_NAME . '2', namespace: CLASS_NAMESPACE . '1');
-    $class3 = new Classs(name: CLASS_NAME . '3', namespace: CLASS_NAMESPACE . '2');
-    $class4 = new Classs(name: CLASS_NAME . '4', namespace: CLASS_NAMESPACE . '2');
+    $class1 = (new Classs(name: CLASS_NAME . '1', namespace: CLASS_NAMESPACE . '1'))
+        ->withStyleClass('classDef0')
+    ;
+    $class2 = (new Classs(name: CLASS_NAME . '2', namespace: CLASS_NAMESPACE . '1'))
+        ->withStyleClass('classDef2')
+    ;
+    $class3 = (new Classs(name: CLASS_NAME . '3', namespace: CLASS_NAMESPACE . '2'))
+        ->withStyleClass('classDef1')
+    ;
+    $class4 = (new Classs(name: CLASS_NAME . '4', namespace: CLASS_NAMESPACE . '2'));
 
     $diagram = (new ClassDiagram(title: TITLE))
         ->withNote(new Note(NOTE))
@@ -130,6 +136,17 @@ test('classDiagram with everything', function () {
             new Relationship($class2, $class3, RelationshipType::Inheritance),
             new Relationship($class2, $class4, RelationshipType::Inheritance)
         )
+        ->withClassDef([
+            'classDef0' => 'fill:white',
+            'classDef1' => ['font-style' => 'italic']
+        ])
+        ->addClassDef(['classDef2' => [
+            'fill' => '#f00',
+            'color' => 'white',
+            'font-weight' => 'bold',
+            'stroke-width' => '2px',
+            'stroke' => 'yellow'
+        ]])
     ;
 
     expect($diagram->render())
@@ -139,13 +156,13 @@ test('classDiagram with everything', function () {
             . "---\n"
             . "classDiagram\n"
             . '  namespace ' . CLASS_NAMESPACE . "1 {\n"
-            . '    class ' . CLASS_NAME . "1 {\n"
+            . '    class ' . CLASS_NAME . "1:::classDef0 {\n"
             . "    }\n"
-            . '    class ' . CLASS_NAME . "2 {\n"
+            . '    class ' . CLASS_NAME . "2:::classDef2 {\n"
             . "    }\n"
             . "  }\n"
             . '  namespace ' . CLASS_NAMESPACE . "2 {\n"
-            . '    class ' . CLASS_NAME . "3 {\n"
+            . '    class ' . CLASS_NAME . "3:::classDef1 {\n"
             . "    }\n"
             . '    class ' . CLASS_NAME . "4 {\n"
             . "    }\n"
@@ -154,6 +171,9 @@ test('classDiagram with everything', function () {
             . '  ' . CLASS_NAME . '2 --|&gt; ' . CLASS_NAME . "3\n"
             . '  ' . CLASS_NAME . '2 --|&gt; ' . CLASS_NAME . "4\n"
             . '  note &quot;' . NOTE . "&quot;\n"
+            . "  classDef classDef0 fill:white;\n"
+            . "  classDef classDef1 font-style:italic;\n"
+            . "  classDef classDef2 fill:#f00,color:white,font-weight:bold,stroke-width:2px,stroke:yellow;\n"
             . '</pre>'
         )
     ;
