30436dadf3bff193c48415034bf24d1ed5087453
Chris <chris@clyates.plus.com>
2026-01-23 19:40:27 +0000

    Add logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,22 +7,24 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Mermaid Class Diagram'
-copyright = '2025, BeastBytes'
+copyright = '2026, BeastBytes'
 author = 'BeastBytes'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinxcontrib.phpdomain']
+extensions = [
+    'sphinxcontrib.mermaid',
+    'sphinxcontrib.phpdomain'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_favicon = 'favicon.png'
+html_logo = 'logo.png'
+html_theme = 'furo'
 html_static_path = ['_static']
